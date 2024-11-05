b4a53db77460763d3448fcdac0ca1dff81c879c8
Welber Costa <welberpcosta@gmail.com>
2024-11-04 22:07:57 -0300

    Commit numb3 - construcao do componente service
    Co-authored-by: Igor Suzart <is-suzart@users.noreply.github.com>

---
+++ src/app/_components/services.tsx
@@ -0,0 +1,32 @@
+export function Services (){
+    return(
+        <div className="bg-orange-500 rounded px-[10vw] py-10">service 
+            <div className="text-center">
+                <h1 className="text-black font-black text-[40px] ">Services</h1>
+              <p className="text-white px-[10%]">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa nemo repudiandae quas error ducimus obcaecati voluptates veniam magnam saepe, accusamus enim quo, odio, natus vitae. 
+                Illum unde eius dolor nostrum?
+              </p>
+                
+            </div>
+            <div>
+                <div className="card">painel 1
+                    <div className="btn-yellow">
+                        Learn more
+                        
+                    </div>
+                </div>
+
+                <div className="card">painel 2 
+                    <div className="btn-yellow">
+                        Learn more
+                    </div>
+                </div>
+                
+            </div>flex
+         oi!
+        </div>
+
+    );
+
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 
 import { Leading } from "./_components/leading";
 import { About } from "./_components/about";
+import { Services } from "./_components/services";
 
 export default function Home() {
   return (
     <main >
       <Leading />
       <About />
+      <Services />
     </main>
   );
 }
